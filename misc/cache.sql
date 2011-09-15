a13a47bafa6565184b61f940e6a17297e113ee6b
Dan Stillman <dstillman@zotero.org>
2011-09-15 17:14:16 +0000

    Add an index on the download cache table, and purge more frequently

--- misc/cache.sql
+++ misc/cache.sql
@@ -1,10 +1,11 @@
-CREATE TABLE IF NOT EXISTS `syncDownloadCache` (
+CREATE TABLE `syncDownloadCache` (
   `hash` char(32) NOT NULL,
   `userID` int(10) unsigned NOT NULL,
   `lastsync` varchar(20) NOT NULL,
   `xmldata` longblob NOT NULL,
   `lastUsed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`hash`)
+  PRIMARY KEY (`hash`),
+  KEY `lastUsed` (`lastUsed`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE EVENT purgeCache ON SCHEDULE EVERY 24 HOUR DO DELETE FROM syncDownloadCache WHERE lastUsed<NOW() - INTERVAL 2 WEEK;
+CREATE EVENT purgeCache ON SCHEDULE EVERY 8 HOUR DO DELETE FROM syncDownloadCache WHERE lastUsed<NOW() - INTERVAL 2 WEEK;
