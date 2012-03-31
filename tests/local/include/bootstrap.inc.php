cd763eaec787f3fa02976e70d30e6b6f9f2b1f20
Dan Stillman <dstillman@zotero.org>
2012-03-31 03:30:44 -0400

    Allow unit tests only on testing site, remove old file, add config file

--- tests/local/include/bootstrap.inc.php
+++ tests/local/include/bootstrap.inc.php
@@ -1,3 +1,7 @@
 <?
 set_include_path(get_include_path() . PATH_SEPARATOR . "../../include");
 require_once("header.inc.php");
+
+if (!Z_ENV_TESTING_SITE) {
+	throw new Exception("Tests can be run only on testing site");
+}
