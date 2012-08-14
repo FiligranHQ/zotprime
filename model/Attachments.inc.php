16766d85de7afc13322a609a40be9d604c3f9eb8
Dan Stillman <dstillman@zotero.org>
2012-08-14 00:06:44 -0400

    Add some missing copyright notices

--- model/Attachments.inc.php
+++ model/Attachments.inc.php
@@ -1,4 +1,29 @@
-<?
+<?php
+/*
+    ***** BEGIN LICENSE BLOCK *****
+    
+    This file is part of the Zotero Data Server.
+    
+    Copyright © 2011 Center for History and New Media
+                     George Mason University, Fairfax, Virginia, USA
+                     http://zotero.org
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
 class Zotero_Attachments {
 	private static $cacheTime = 60; // seconds to cache extracted ZIP files
 	
