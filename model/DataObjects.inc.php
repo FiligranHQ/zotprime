6b2bec12cef280a0ebd05c55ba4bdff6e0c1ab0c
Dan Stillman <dstillman@zotero.org>
2013-11-05 01:00:32 +0000

    Delete fulltextContent row when attachment item is deleted

--- model/DataObjects.inc.php
+++ model/DataObjects.inc.php
@@ -621,7 +621,6 @@ class Zotero_DataObjects {
 		
 		Zotero_DB::beginTransaction();
 		
-		
 		// Delete child items
 		if ($type == 'item') {
 			if ($obj->isRegularItem()) {
@@ -643,6 +642,11 @@ class Zotero_DataObjects {
 		// Tag deletions need to stored by tag for the API
 		else if ($type == 'tag') {
 			$tagName = $obj->name;
+		}
+		
+		if ($type == 'item' && $obj->isAttachment()) {
+			$sql = "DELETE FROM fulltextContent WHERE libraryID=? AND `key`=?";
+			Zotero_FullText_DB::query($sql, [$libraryID, $key], $shardID);
 		}
 		
 		if ($type == 'relation') {
