54bceef6459a857851decfa179ebe6316f0b0900
Dan Stillman <dstillman@zotero.org>
2014-02-26 06:26:36 -0500

    Allow longer params for classic full-text syncing

--- misc/master.sql
+++ misc/master.sql
@@ -338,7 +338,7 @@ CREATE TABLE `syncDownloadQueue` (
   `lastsync` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `lastsyncMS` smallint(5) unsigned NOT NULL DEFAULT '0',
   `version` smallint(5) unsigned NOT NULL,
-  `params` text NOT NULL,
+  `params` mediumtext NOT NULL,
   `added` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `objects` int(10) unsigned NOT NULL,
   `lastCheck` timestamp NULL DEFAULT NULL,
