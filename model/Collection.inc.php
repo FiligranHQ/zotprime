FiligranHQ/zotprime

commit 8e8bd9fc2cc5a7ba7f80a131dbd91261a4a9235e

Fix error adding or removing a huge number of collection items

model/Collection.inc.php
@@ -397,27 +397,30 @@ class Zotero_Collection extends Zotero_DataObject {
 		
 		if ($removed) {
 			$sql = "DELETE FROM collectionItems WHERE collectionID=? AND itemID IN (";
-			$q = array();
-			$params = array($this->id);
-			foreach ($removed as $itemID) {
-				$q[] = '?';
-				$params[] = $itemID;
+			while ($chunk = array_splice($removed, 0, 500)) {
+				array_unshift($chunk, $this->id);
+				Zotero_DB::query(
+					$sql . implode(', ', array_fill(0, sizeOf($chunk) - 1, '?')) . ")",
+					$chunk,
+					$shardID
+				);
 			}
-			$sql .= implode(',', $q) . ")";
-			Zotero_DB::query($sql, $params, $shardID);
 		}
 		
 		if ($new) {
-			$sql = "INSERT INTO collectionItems (collectionID, itemID) VALUES";
-			$q = array();
-			$params = array();
-			foreach ($new as $itemID) {
-				$q[] = '(?,?)';
-				$params = array_merge($params,
-					array($this->id, $itemID));
+			$sql = "INSERT INTO collectionItems (collectionID, itemID) VALUES ";
+			while ($chunk = array_splice($new, 0, 250)) {
+				Zotero_DB::query(
+					$sql . implode(',', array_fill(0, sizeOf($chunk), '(?,?)')),
+					call_user_func_array(
+						'array_merge',
+						array_map(function ($itemID) {
+							return [$this->id, $itemID];
+						}, $chunk)
+					),
+					$shardID
+				);
 			}
-			$sql .= implode(',', $q);
-			Zotero_DB::query($sql, $params, $shardID);
 		}
 		
 		$this->items = array_values(array_unique($itemIDs));

commit 6d10a9998fab929824925fdedba8079714132c20

Fix item version update on collection-item change via classic sync
Broken by 8e8bd9fc2c

model/Collection.inc.php
@@ -396,8 +396,9 @@ class Zotero_Collection extends Zotero_DataObject {
 		$new = array_diff($itemIDs, $current);
 		
 		if ($removed) {
+			$arr = $removed;
 			$sql = "DELETE FROM collectionItems WHERE collectionID=? AND itemID IN (";
-			while ($chunk = array_splice($removed, 0, 500)) {
+			while ($chunk = array_splice($arr, 0, 500)) {
 				array_unshift($chunk, $this->id);
 				Zotero_DB::query(
 					$sql . implode(', ', array_fill(0, sizeOf($chunk) - 1, '?')) . ")",
@@ -408,8 +409,9 @@ class Zotero_Collection extends Zotero_DataObject {
 		}
 		
 		if ($new) {
+			$arr = $new;
 			$sql = "INSERT INTO collectionItems (collectionID, itemID) VALUES ";
-			while ($chunk = array_splice($new, 0, 250)) {
+			while ($chunk = array_splice($arr, 0, 250)) {
 				Zotero_DB::query(
 					$sql . implode(',', array_fill(0, sizeOf($chunk), '(?,?)')),
 					call_user_func_array(