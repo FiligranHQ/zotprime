FiligranHQ/zotprime

commit b824ea425f2932bf4c0c44fb00e5d9250e8f8c5c

Fix display of legacy My Publications library items

model/Items.inc.php
@@ -68,8 +68,13 @@ class Zotero_Items {
 		
 		$shardID = Zotero_Shards::getByLibraryID($libraryID);
 		
+		$isPublications = !empty($params['publications']);
+		if ($params['publications'] && Zotero_Libraries::getType($libraryID) == 'publications') {
+			$isPublications = false;
+		}
+		
 		$includeNotes = true;
-		if (empty($params['publications']) && $permissions && !$permissions->canAccess($libraryID, 'notes')) {
+		if ($isPublications && $permissions && !$permissions->canAccess($libraryID, 'notes')) {
 			$includeNotes = false;
 		}
 		
@@ -178,7 +183,7 @@ class Zotero_Items {
 			}
 		}
 		
-		if (!empty($params['publications'])) {
+		if ($isPublications) {
 			$sql .= "LEFT JOIN publicationsItems PI ON (PI.itemID=I.itemID) ";
 		}
 		
@@ -291,7 +296,7 @@ class Zotero_Items {
 			}
 		}
 		
-		if (!empty($params['publications'])) {
+		if ($isPublications) {
 			$sql .= "AND PI.itemID IS NOT NULL ";
 		}
 		

commit 85d062080fd6f4356a2de163cc5f331a1bd0129f

Fix child note access in non-publications mode
Regression from b824ea425

model/Items.inc.php
@@ -74,7 +74,7 @@ class Zotero_Items {
 		}
 		
 		$includeNotes = true;
-		if ($isPublications && $permissions && !$permissions->canAccess($libraryID, 'notes')) {
+		if (!$isPublications && $permissions && !$permissions->canAccess($libraryID, 'notes')) {
 			$includeNotes = false;
 		}
 		