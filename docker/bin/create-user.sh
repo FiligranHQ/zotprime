e42371b546f281641097a2d38b63707f6cbf5b51
Samuel Hassine <samuel.hassine@ssi.gouv.fr>
2018-10-25 14:26:00 +0200

    Zotero dataserver configuration

--- docker/bin/create-user.sh
+++ docker/bin/create-user.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-MYSQL="mysql -h mysql -P 3306 -u root -pzotero"
-
-echo "INSERT INTO libraries VALUES (${1}, 'user', '0000-00-00 00:00:00', 0, 1)" | $MYSQL zotero_master
-echo "INSERT INTO users VALUES (${1}, ${1}, '${2}', '0000-00-00 00:00:00', '0000-00-00 00:00:00')" | $MYSQL zotero_master
-echo "INSERT INTO groupUsers VALUES (1, ${1}, 'member', '0000-00-00 00:00:00', '0000-00-00 00:00:00')" | $MYSQL zotero_master
-echo "INSERT INTO users VALUES (${1}, '${2}', MD5('${2}'))" | $MYSQL zotero_www
-echo "INSERT INTO users_email (userID, email) VALUES (1, '${2}@ops.fr')" | $MYSQL zotero_www
-echo "INSERT INTO shardLibraries VALUES (${1}, 'user', 0, 0)" | $MYSQL zotero_shard_1
-
+docker-compose exec app-zotero /var/www/zotero/admin/create-user.sh ${1} ${2}
