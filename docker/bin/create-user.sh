b203adb00c72b768b98d4240a481f43ea04be2a3
Francesco Versaci <francesco.versaci@gmail.com>
2019-01-29 17:32:01 +0100

    Added optional email parameter when creating user

--- docker/bin/create-user.sh
+++ docker/bin/create-user.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
-	echo "Usage: ./create-user.sh {UID} {username} {password}"
+	echo "Usage: ./create-user.sh {UID} {username} {password} [email]"
 	exit 1
 fi
 
-sudo docker-compose exec app-zotero /var/www/zotero/admin/create-user.sh ${1} ${2} ${3}
+sudo docker-compose exec app-zotero /var/www/zotero/admin/create-user.sh ${1} ${2} ${3} ${4}
