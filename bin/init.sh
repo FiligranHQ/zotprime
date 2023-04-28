afe1080e3e77e7b4fb9f284b35ea7b13dda722b5
ALEXANDER QUADRATUS /Ἀλέξανδρος Κοδρᾶτος/ <uniuuu@vivaldi.net>
2023-04-28 20:08:17 +0700

    Draft: Added script to apply db_updates

--- bin/init.sh
+++ bin/init.sh
@@ -3,6 +3,7 @@
 set -eux
 
 sudo docker compose exec app-zotprime-dataserver bash -cux 'cd /var/www/zotero/misc && ./init-mysql.sh'
+#sudo docker compose exec app-zotprime-dataserver bash -cux 'cd /var/www/zotero/misc && ./db_update.sh'
 sudo docker compose exec app-zotprime-dataserver bash -cux 'aws --endpoint-url "http://minio:9000" s3 mb s3://zotero'
 sudo docker compose exec app-zotprime-dataserver bash -cux 'aws --endpoint-url "http://minio:9000" s3 mb s3://zotero-fulltext'
 sudo docker compose exec app-zotprime-dataserver bash -cux 'aws --endpoint-url "http://localstack:4575" sns create-topic --name zotero'
