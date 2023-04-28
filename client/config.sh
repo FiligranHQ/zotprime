e45a35498cb7acd24eff45efa3210d51737e86ce
ALEXANDER QUADRATUS /Ἀλέξανδρος Κοδρᾶτος/ <uniuuu@vivaldi.net>
2023-04-28 20:29:34 +0700

    fix client config

--- client/config.sh
+++ client/config.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -eux
+
+sed -i "s#'http://zotero.org/'#'http://localhost:8080/'#g" zotero-client/resource/config.js 
+sed -i "s#'zotero.org'#'localhost'#g" zotero-client/resource/config.js
 sed -i 's#https://api.zotero.org/#http://localhost:8080/#g' zotero-client/resource/config.js
 sed -i 's#wss://stream.zotero.org/#ws://localhost:8081/#g' zotero-client/resource/config.js
 sed -i 's#https://www.zotero.org/#http://localhost:8080/#g' zotero-client/resource/config.js
